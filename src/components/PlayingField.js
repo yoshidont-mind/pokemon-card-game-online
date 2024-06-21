651734b5eca66bb317ba8ebcb94b3ce468f2a381
yoshidont-mind <tyoshida6@my.bcit.ca>
2024-06-21 19:45:17 +0900

    Fix placement of PlayingField

--- src/components/PlayingField.js
+++ src/components/PlayingField.js
@@ -27,76 +27,74 @@ const PlayingField = ({ sessionId, playerId }) => {
     }
 
     return (
-        <div className={`game-board container mt-5 ${styles.playingField}`}>
-            <div className="row mb-4">
-                <div className="col-12 text-center">
-                    <div className={`${styles.message}`}>{gameData.message}</div>
-                </div>
-            </div>
-            <div className="row mb-4">
-                <div className="col-2 text-center">
-                    <div className={`opponent ${styles.deck}`}>山札（相手）</div>
-                </div>
-                <div className="col-2 text-center">
-                    <div className={`opponent ${styles.trash}`}>トラッシュ（相手）</div>
-                </div>
-                <div className="col-2 text-center">
-                    <div className={`opponent ${styles.stadium}`}>スタジアム（相手）</div>
-                </div>
-                <div className="col-6 text-center">
-                    <div className={`opponent ${styles.bench}`}>
-                        {opponentData.bench?.map((pokemon, index) => (
-                            <Pokemon key={index} {...pokemon} />
-                        ))}
+        <div className={`game-board container mt-5 text-center ${styles.playingField}`}>
+            <div id="opponentField">
+                <div className="row mb-4">
+                    <div className={`col-3`}>
+                        <div className="">
+                            <div className={`opponent ${styles.discardPile}`}>トラッシュ（相手）</div>
+                        </div>
+                        <div className="">
+                            <div className={`opponent ${styles.deck}`}>山札（相手）</div>
+                        </div>
+                    </div>
+                    <div className={"col-6"}>
+                        <div className={`opponent ${styles.bench}`}>
+                            {opponentData.bench?.map((pokemon, index) => (
+                                <Pokemon key={index} {...pokemon} />
+                            ))}ベンチ（相手）
+                        </div>
+                        <div className={`row`}>
+                            <div className="col-4">
+                                <div className={`opponent ${styles.stadium}`}>スタジアム（相手）</div>
+                            </div>
+                            <div className="col-4">
+                                <div className={`opponent ${styles.activeSpot}`}>バトルポケモン（相手）</div>
+                            </div>
+                            <div className="col-4">
+                                <div className={`opponent ${styles.prizeCards}`}>サイド（相手）</div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-3">
+                        <div className={`opponent ${styles.message}`}>{playerData.message}</div>
                     </div>
                 </div>
             </div>
-            <div className="row mb-4">
-                <div className="col-2 text-center">
-                    <div className={`opponent ${styles.side}`}>サイド（相手）</div>
-                </div>
-                <div className="col-4 text-center">
-                    {opponentData.activeSpot && <Pokemon className="active-spot opponent" {...opponentData.activeSpot} />}
-                </div>
-                <div className="col-4 text-center">
-                    {playerData.activeSpot && <Pokemon className="active-spot self" {...playerData.activeSpot} />}
-                </div>
-                <div className="col-2 text-center">
-                    <div className={`self ${styles.side}`}>サイド</div>
-                </div>
-            </div>
-            <div className="row mb-4">
-                <div className="col-2 text-center">
-                    <div className={`self ${styles.deck}`}>山札</div>
-                </div>
-                <div className="col-2 text-center">
-                    <div className={`self ${styles.trash}`}>トラッシュ</div>
-                </div>
-                <div className="col-2 text-center">
-                    <div className={`self ${styles.stadium}`}>スタジアム</div>
-                </div>
-                <div className="col-6 text-center">
-                    <div className={`self ${styles.bench}`}>
-                        {playerData.bench?.map((pokemon, index) => (
-                            <Pokemon key={index} {...pokemon} />
-                        ))}
+            <div id="playerField">
+                <div className="row">
+                    <div className={`col-3 self ${styles.hand}`}>
+                        <div>手札</div>
+                        <div className="action-buttons">
+                            <button className="btn btn-primary m-1">見せる</button>
+                            <button className="btn btn-primary m-1">ベンチに出す</button>
+                            <button className="btn btn-primary m-1">バトル場に出す</button>
+                            <button className="btn btn-primary m-1">山札に戻す</button>
+                            <button className="btn btn-primary m-1">トラッシュ</button>
+                            <button className="btn btn-primary m-1">スタジアムに出す</button>
+                        </div>
                     </div>
-                </div>
-            </div>
-            <div className="row mb-4">
-                <div className="col-12 text-center">
-                    <div className={`self ${styles.hand}`}>手札</div>
-                </div>
-            </div>
-            <div className="row mb-4">
-                <div className="col-12 text-center">
-                    <div className="action-buttons">
-                        <button className="btn btn-primary m-1">見せる</button>
-                        <button className="btn btn-primary m-1">ベンチに出す</button>
-                        <button className="btn btn-primary m-1">バトル場に出す</button>
-                        <button className="btn btn-primary m-1">山札に戻す</button>
-                        <button className="btn btn-primary m-1">トラッシュ</button>
-                        <button className="btn btn-primary m-1">スタジアムに出す</button>
+                    <div className="col-6">
+                        <div className="row">
+                            <div className="col-4">
+                                <div className={`self ${styles.prizeCards}`}>サイド</div>
+                            </div>
+                            <div className="col-4">
+                                <div className={`self ${styles.activeSpot}`}>バトルポケモン（自分）</div>
+                            </div>
+                            <div className="col-4">
+                                <div className={`self ${styles.stadium}`}>スタジアム</div>
+                            </div>
+                        </div>
+                        <div className={`self ${styles.bench}`}>
+                            {playerData.bench?.map((pokemon, index) => (
+                                <Pokemon key={index} {...pokemon} />
+                            ))}ベンチ
+                        </div>
+                    </div>
+                    <div className="col-3">
+                        <div className={`self ${styles.deck}`}>山札</div>
+                        <div className={`self ${styles.discardPile}`}>トラッシュ</div>
                     </div>
                 </div>
             </div>
